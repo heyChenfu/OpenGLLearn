47633aee4e26f04f6c8213583dad47f0dd5369b9
陈赋 <1576391213@qq.com>
2020-12-10 11:30:08 +0800

    使用纹理

--- src/HelloOpenGL.cpp
+++ src/HelloOpenGL.cpp
@@ -2,9 +2,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <Windows.h>
 #include "CommonFunction.h"
 #include "Shader.h"
-#include <Windows.h>
+#include "stb_imageImplement.h"
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
@@ -36,21 +37,46 @@ unsigned int GenerateVAO(float * vertices, int verticeslen, unsigned int * indic
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicesLen, indices, GL_STATIC_DRAW);
     // 设置顶点属性指针
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     // 颜色属性
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+    //纹理属性
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
     //解除绑定
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     return VAO;
 }
 
+unsigned int GenerateTexture(std::string sTexturePath) {
+    unsigned int texture = 0;
+    glGenTextures(1/*生成的纹理数目*/, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    //加载图片
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true); //OpenGL要求y轴0.0坐标是在图片的底部的，但是图片的y轴0.0坐标通常在顶部
+    unsigned char* data = stbi_load(sTexturePath.c_str(), &width, &height, &nrChannels, 0);
+    if (data == nullptr)
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    else {
+        //载入的图片数据
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D); //为当前绑定的纹理自动生成所有需要的多级渐远纹理
+        //释放图像的内存是一个很好的习惯
+        stbi_image_free(data);
+    }
+    return texture;
+}
+
 int main()
 {
-    //------------------------------------------------------
+    //-----------------------------------
     // glfw 窗口创建
-    //------------------------------------------------------
+    //-----------------------------------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -76,23 +102,40 @@ int main()
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    //----------------------------------
+    // shader
+    //----------------------------------
     std::string vertexPath = "shader/vertexShader.txt";
     std::string fragmentPath = "shader/fragmentShader.txt";
     Shader * shaderClass = new Shader(vertexPath.c_str(), fragmentPath.c_str());
 
-    //------------------------------------------------------
+    //----------------------------------
     //设置顶点数据(和缓冲区)和配置顶点属性
-    //------------------------------------------------------
+    //----------------------------------
     float vertices[] = {
-        // 位置              // 颜色
-         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // 右下
-        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // 左下
-         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // 顶部
+        //     ---- 位置 ----     ---- 颜色 ----      - 纹理坐标 -
+         0.5f,  0.5f, 0.0f,         1.0f, 0.0f, 0.0f,       1.0f, 1.0f,   // 右上
+         0.5f, -0.5f, 0.0f,         0.0f, 1.0f, 0.0f,       1.0f, 0.0f,   // 右下
+        -0.5f, -0.5f, 0.0f,         0.0f, 0.0f, 1.0f,       0.0f, 0.0f,   // 左下
+        -0.5f,  0.5f, 0.0f,         1.0f, 1.0f, 0.0f,       0.0f, 1.0f    // 左上
     };
     unsigned int indices[] = { // 注意索引从0开始! 
-    0, 1, 2, // 第一个三角形
+    0, 1, 2,
+    2, 3, 0
     };
     unsigned int VAO = GenerateVAO(vertices, sizeof(vertices), indices, sizeof(indices));
+
+    //----------------------------------
+    //生成纹理
+    //----------------------------------
+    //创建第一个纹理
+    unsigned int texture1 = GenerateTexture("res/container.jpg");
+    //创建第二个纹理
+    unsigned int texture2 = GenerateTexture("res/awesomeface.jpg");
+    //设置采样
+    shaderClass->use(); // 不要忘记在设置uniform变量之前激活着色器程序！
+    shaderClass->setInt("texture1", 0);
+    shaderClass->setInt("texture2", 1);
 
     //渲染循环
     while (!glfwWindowShouldClose(window))
@@ -107,15 +150,19 @@ int main()
         //---------------------------------
         //draw
         //---------------------------------
-        glUseProgram(shaderClass->ID);
+        glActiveTexture(GL_TEXTURE0); // 在绑定纹理之前先激活纹理单元, 0默认总是被激活
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+
+        shaderClass->use();
         glBindVertexArray(VAO);
         //glDrawArrays(GL_TRIANGLES, 0, 6);
-        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
 
+        glfwSwapBuffers(window); //交换缓冲
         glfwPollEvents();
-        //交换缓冲
-        glfwSwapBuffers(window);
     }
 
     //删除分配的所有资源
